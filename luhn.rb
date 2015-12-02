627b42d03463f5dc2d7db528232fe7fa5a1c7a35
Chip Self <chipself9@gmail.com>
2015-12-02 16:49:16 -0500

    changed create method

--- luhn.rb
+++ luhn.rb
@@ -1,13 +1,13 @@
 class Luhn
 
   def initialize luhn
-    @luhn = luhn
+    @luhn = luhn.to_s
   end
 
   def addends
     index = 0
     output = ''
-    @luhn.to_s.reverse.each_char do |char|
+    @luhn.reverse.each_char do |char|
       char = char.to_i * 2 >= 10 ?
       ((char.to_i * 2) - 9).to_s : (char.to_i * 2).to_s if index % 2 != 0
       output = output + char
@@ -25,11 +25,9 @@ class Luhn
   end
 
   def self.create(number)
-    check_digit = 0
-    while check_digit < 10 do
+    0.upto(9) do |check_digit|
       luhn = Luhn.new(number.to_s + check_digit.to_s)
       return (number.to_s + check_digit.to_s).to_i if luhn.valid?
-      check_digit += 1
     end
   end
 end
